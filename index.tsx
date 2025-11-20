056fbdedc0028069063d00c372e3a6ea94ecb014
amustafa91 <a.m.mustafa91@gmail.com>
2025-11-20 14:13:02 +0300

    feat: Add GTM injection to `index.tsx` and implement a GitHub Pages deployment workflow.

--- index.tsx
+++ index.tsx
@@ -1,3 +1,4 @@
+// index.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -9,6 +10,24 @@ if (!rootElement) {
 }
 
 const root = ReactDOM.createRoot(rootElement);
+
+// Inject GTM dynamically
+const gtmId = import.meta.env.VITE_GTM_ID;
+if (gtmId) {
+  const script = document.createElement('script');
+  script.async = true;
+  script.innerHTML = `
+    (function(w,d,s,l,i){
+      w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+      var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+      j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+      f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','${gtmId}');
+  `;
+  document.head.appendChild(script);
+}
+
 root.render(
   <React.StrictMode>
     <App />
