56b545c7d9c5bba273313f2705e844c5d46e29f2
amustafa91 <a.m.mustafa91@gmail.com>
2025-11-27 17:53:57 +0300

    feat: Add new SVG icon components, update the favicon path, and include Vite client types.

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const LogoIcon = (props: React.HTMLAttributes<HTMLImageElement>) => (
   // <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
   // </svg>
-  <img src="./Assets/FF.png" alt="Logo" {...props} />
+  <img src={`${import.meta.env.BASE_URL}assets/ff.png`} alt="Logo" {...props} />
 
 );
 
